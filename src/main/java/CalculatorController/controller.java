68f7f81c5d23bdb6a990750749d30cae30701d41
Khal01 <aliyev_56@list.ru>
2023-12-24 17:39:11 +0300

    spring boot web

--- src/main/java/CalculatorController/controller.java
+++ src/main/java/CalculatorController/controller.java
@@ -23,7 +23,7 @@ public class controller {
 
     }
     @GetMapping("/calculator/minus")
-    public String minus (@RequestParam Integer num1, @RequestParam Integer num2){
+    public String minus (@RequestParam(required = false) Integer num1, @RequestParam(required = false) Integer num2){
         if (num1 == null || num2 == null) {
             return "Один из аргументов не передан ! ";
         }
